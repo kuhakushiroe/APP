e51d37ac894331d17fb3fc9cdd508b3aec778c67
kuhakushiroe <arifinwhite@gmail.com>
2025-05-14 19:30:57 +0800

    error-history-mcu

--- resources/views/livewire/histori/mcu/mcu.blade.php
+++ resources/views/livewire/histori/mcu/mcu.blade.php
@@ -34,35 +34,41 @@
                         <td>
                             @php
                                 $subItems = DB::table('mcu')
-                                    ->whereAny(['id', 'sub_id'], 'like', $data->sub_id)
+                                    ->whereAny(['id', 'sub_id'], '=', $data->sub_id)
                                     ->where('id', '!=', $data->id_mcu)
                                     ->orderBy('tgl_mcu', 'asc') // Mengurutkan berdasarkan tanggal MCU
                                     ->get();
                             @endphp
-                            @foreach ($subItems as $item)
-                                <a class="btn btn-outline-primary btn-sm" href="{{ Storage::url($item->file_mcu) }}"
+                            <div class="d-grid gap-2">
+                                @forelse ($subItems as $index => $item)
+                                    <a class="btn btn-outline-primary btn-sm" href="{{ Storage::url($item->file_mcu) }}"
+                                        target="_blank">
+                                        <span class="bi bi-file-earmark-arrow-down"></span> File MCU
+                                        {{ $index + 1 }}
+                                    </a>
+                                @empty
+                                @endforelse
+                                <a class="btn btn-outline-primary btn-sm" href="{{ Storage::url($data->file_mcu) }}"
                                     target="_blank">
-                                    <span class="bi bi-file-earmark-arrow-down"></span> File MCU
+                                    <span class="bi bi-file-earmark-arrow-down"></span> File MCU Final
                                 </a>
-                            @endforeach
-                            <a class="btn btn-outline-primary btn-sm" href="{{ Storage::url($data->file_mcu) }}"
-                                target="_blank">
-                                <span class="bi bi-file-earmark-arrow-down"></span> File MCU Final
-                            </a>
+                            </div>
                         </td>
                         <td>
-                            @if ($data->mcuStatus == 'FIT' || $data->mcuStatus == 'FIT WITH NOTE')
-                                <a href="cetak-mcu/{{ $data->id_mcu }}" target="_blank"
-                                    class="btn btn-outline-warning btn-sm">
-                                    <span class="bi bi-download"></span>
-                                    Download Verifikasi Final {{ $data->mcuStatus }}
-                                </a>
-                                <a href="cetak-skd/{{ $data->id_mcu }}" class="btn btn-outline-success btn-sm"
-                                    target="_blank">
-                                    <span class="bi bi-download"></span>
-                                    Download SKD {{ $data->mcuStatus }}
-                                </a>
-                            @endif
+                            <div class="d-grid gap-2">
+                                @if ($data->mcuStatus == 'FIT' || $data->mcuStatus == 'FIT WITH NOTE')
+                                    <a href="cetak-mcu/{{ $data->id_mcu }}" target="_blank"
+                                        class="btn btn-outline-warning btn-sm">
+                                        <span class="bi bi-download"></span>
+                                        Download Verifikasi Final {{ $data->mcuStatus }}
+                                    </a>
+                                    <a href="cetak-skd/{{ $data->id_mcu }}" class="btn btn-outline-success btn-sm"
+                                        target="_blank">
+                                        <span class="bi bi-download"></span>
+                                        Download SKD {{ $data->mcuStatus }}
+                                    </a>
+                                @endif
+                            </div>
                         </td>
                         <td>
                             {{ \Carbon\Carbon::parse($data->exp_mcu)->locale('id')->translatedFormat('l, d F Y') }}
