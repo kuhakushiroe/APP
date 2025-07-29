9ccd8578ce8530b042fb22c776b30908a44ecf93
kuhakushiroe <arifinwhite@gmail.com>
2025-07-30 03:35:49 +0700

    Perbaikan Duplikat WEb Route

--- routes/web.php
+++ routes/web.php
@@ -33,36 +33,37 @@ use Maatwebsite\Excel\Facades\Excel;
 Route::group(['middleware' => ['auth']], function () {
     Route::get('/', Home::class)->name('home');
     Route::get('profil', Password::class)->name('profil');
-    Route::group(['middleware' => ['role:superadmin,admin,she']], function () {
-        Route::get('karyawan', Karyawan::class)->name('karyawan');
-    });
-    Route::get('profil', Password::class)->name('profil');
     Route::get('mcu', Mcu::class)->name('mcu');
     Route::get('histori-mcu', McuMcu::class)->name('histori-mcu');
     Route::get('report', Report::class)->name('report');
 
+    Route::group(['middleware' => ['role:superadmin,admin,she']], function () {
+        //master data
+        Route::get('karyawan', Karyawan::class)->name('karyawan');
+        Route::get('pengajuan-id', Id::class)->name('pengajuan-id');
+        Route::get('pengajuan-kimper', Kimper::class)->name('pengajuan-kimper');
+        Route::get('histori-id', HistoriId::class)->name('histori-id');
+        Route::get('histori-kimper', HistoriKimper::class)->name('histori-kimper');
+    });
     Route::group(['middleware' => ['role:superadmin']], function () {
         //Route::get('export-karyawans', [Karyawan::class, 'export'])->name('export-karyawans');
+        //full master data
         Route::get('users', Users::class)->name('users');
         Route::get('departments', Departments::class)->name('departments');
         Route::get('perusahaan', Perusahaan::class)->name('perusahaan');
         Route::get('jabatan', Jabatan::class)->name('jabatan');
         Route::get('versatility', Versatility::class)->name('versatility');
     });
-    Route::group(['middleware' => ['role:superadmin,admin,she']], function () {
-        Route::get('pengajuan-id', Id::class)->name('pengajuan-id');
-        Route::get('pengajuan-kimper', Kimper::class)->name('pengajuan-kimper');
-        Route::get('histori-id', HistoriId::class)->name('histori-id');
-        Route::get('histori-kimper', HistoriKimper::class)->name('histori-kimper');
-    });
     Route::group(['middleware' => ['role:superadmin,she']], function () {
+        //Cetak Kartu
         Route::get('cetak-id', CetakId::class)->name('id-karyawan');
         Route::get('cetak-kimper', CetakKimper::class)->name('kimper-karyawan');
         Route::get('cetak-kartu/{id}', [CetakKartuController::class, 'cetak']);
         Route::get('cetak-kartu-kimper/{id}', [CetakKartuController::class, 'cetakKimper']);
     });
-    Route::group(['middleware' => ['role:superadmin,dokter']], function () {
-        Route::get('report-mcu/{date_mcu1}/{date_mcu2}', [Report::class, 'mcuReport'])->name('report-mcu');
+    Route::group(['middleware' => ['role:admin,superadmin,dokter']], function () {
+        //Cetak MCU Dokter tidak bisa lihat report
+        //Route::get('report-mcu/{date_mcu1}/{date_mcu2}', [Report::class, 'mcuReport'])->name('report-mcu');
         Route::get('export-hasil-mcu', [Mcu::class, 'exportExcel'])->name('export-hasil-mcu');
         Route::get('cetak-mcu/{id}', [McuCetak::class, 'cetak']);
         Route::get('cetak-mcu-sub/{id}', [McuCetak::class, 'cetakSub']);
@@ -70,7 +71,9 @@ Route::group(['middleware' => ['auth']], function () {
         Route::get('cetak-laik/{id}', [McuCetak::class, 'cetakLaik']);
     });
     Route::group(['middleware' => ['role:admin,superadmin']], function () {
+        //Cetak MCU
         Route::get('report-mcu/{date_mcu1}/{date_mcu2}', [Report::class, 'mcuReport'])->name('report-mcu');
+        //Cetak Register
         Route::get('cetak-register-id/{date_id1}/{date_id2}', [CetakRegister::class, 'registerID'])->name('cetak-register-id');
         Route::get('cetak-register-kimper/{date_kimper1}/{date_kimper2}', [CetakRegister::class, 'registerKIMPER'])->name('cetak-register-kimper');
         Route::get('formulir-kimper/{id}', [CetakRegister::class, 'formulirKIMPER'])->name('formulir-kimper');
