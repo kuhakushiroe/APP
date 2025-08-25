ddf7785b179fe40343b2290f59625bd73b4af1f9
kuhakushiroe <arifinwhite@gmail.com>
2025-08-25 16:22:04 +0800

    Test

--- app/Livewire/Mcu/Mcu.php
+++ app/Livewire/Mcu/Mcu.php
@@ -25,8 +25,8 @@ class Mcu extends Component
     use WithPagination, WithoutUrlPagination, WithFileUploads;
 
     public $search = '';
-    public $searchtgl_awal1;
-    public $searchtgl_akhir2;
+    public $searchtgl_awal;
+    public $searchtgl_akhir;
     protected $updatesQueryString = ['search'];
     public $form = false;
     public $form_multi = false;
